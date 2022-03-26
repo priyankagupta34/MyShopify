efef70162dcc5380e3eec8a524fdb71cb6704a91
priyankagupta34 <34priyankagupta@gmail.com>
2022-03-26 22:52:27 +0530

    ok

--- src/js/Done.js
+++ src/js/Done.js
@@ -1,11 +1,9 @@
 export default function Done({ setNotificationMessage }) {
   return (
     <div className="done">
-      <div style={{ color: "black" }}>SUCESS!!</div>
+      <div style={{ color: "black", fontWeight: 800 }}>SUCESS!!</div>
       <div>Your Order is Placed!!</div>
-      <div>
-        It will reach you by 7<sup>TH</sup> June
-      </div>
+      <div>It will reach within XYZ business days!!</div>
       <button
         className="addToCart"
         onClick={() => setNotificationMessage(false)}
